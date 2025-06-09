de9c0f26508107b21cc0be1c15edb36f2a9564a9
El Bronco <2020-0288@uteco.edu.do>
2025-06-09 08:54:04 -0400

    Updated

--- obj/Debug/net8.0/Box.AssemblyInfo.cs
+++ obj/Debug/net8.0/Box.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Box")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2d754f585003571235f72e74eb7ccc8fad189a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215a60f58e9d2e0363447af5e208df28bd7a7721")]
 [assembly: System.Reflection.AssemblyProductAttribute("Box")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Box")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
